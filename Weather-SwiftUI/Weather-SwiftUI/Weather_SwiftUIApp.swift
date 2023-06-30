5ff9a53568ca59978cb24b1e381bc6f5bae31dc5
Jobins John <jobins.john@musashiai.com>
2023-06-30 11:23:34 -0400

    Initial Commit

---
+++ Weather-SwiftUI/Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Musashi Ai on 2023-06-30.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
